1b2ac18518c90bdaf5a7a7baeef077376820ef47
Gavaskar Thavarajah <gavas2007@gmail.com>
2024-08-13 22:46:38 -0400

    Answers for the assignment

--- list2.py
+++ list2.py
@@ -6,8 +6,14 @@
 # modify the passed in list.
 
 def remove_adjacent(nums):
-    # +++your code here+++
-    return
+    if not nums:
+        return []
+    
+    result = [nums[0]]
+    for num in nums[1:]:
+        if num != result[-1]:
+            result.append(num)
+    return result
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -16,7 +22,19 @@ def remove_adjacent(nums):
 # pass of both lists.
 
 def linear_merge(list1, list2):
-    # +++your code here+++
+    result = []
+    i, j = 0, 0
+    
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            result.append(list1[i])
+            i += 1
+        else:
+            result.append(list2[j])
+            j += 1
+    
+    result.extend(list1[i:])
+    result.extend(list2[j:])
     return
 
 
